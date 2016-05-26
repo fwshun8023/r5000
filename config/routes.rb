cc7de0027b1289e00f0e2e98a24989b7c84349df
fanwei <fanwei@netfarmer.com.cn>
2016-05-26 09:43:47 +0800

    home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  root 'home#index'
+
   devise_for :users, path: 'auth', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'cmon_let_me_in' }
   resources :articles
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
